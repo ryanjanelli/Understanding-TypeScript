d0fd92b7015818654608c0619d18ddf7b60396e5
Ryan Janelli <ryanjanelli@gmail.com>
2023-03-28 19:54:52 -0400

    add class-validator

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@
 
 import "reflect-metadata";
 import { plainToClass } from "class-transformer";
+import { validate } from "class-validator";
 
 import { Product } from "./product.model";
 
@@ -10,6 +11,15 @@ const products = [
   { title: "A Carpet", price: 29.99 },
   { title: "A Book", price: 10.99 },
 ];
+
+const newProd = new Product("", -5.99);
+validate(newProd).then((errors) => {
+  if (errors.length > 0) {
+    console.log("VALIDATION ERROR!");
+    console.log(errors);
+  }
+});
+console.log(newProd.getInformation());
 // const p1 = new Product("A book", 12.99);
 
 // manual method without class-transformer
